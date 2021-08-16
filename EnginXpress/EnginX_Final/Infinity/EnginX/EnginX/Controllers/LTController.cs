5bd5c5653860d69aaad0e54c1fdb5c2bd5f53c87
uzairessop <72469425+uzairessop@users.noreply.github.com>
2021-08-16 21:19:25 +0200

    InitialCreate

---
+++ EnginXpress/EnginX_Final/Infinity/EnginX/EnginX/Controllers/LTController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnginX.Controllers
+{
+    public class LTController : Controller
+    {
+        // GET: LT
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
