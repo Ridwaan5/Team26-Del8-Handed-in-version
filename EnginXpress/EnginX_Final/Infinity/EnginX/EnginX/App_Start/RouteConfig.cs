93ac860b75d77eb683fbdb91eca8c15c56fa40ee
uzairessop <72469425+uzairessop@users.noreply.github.com>
2021-08-24 11:37:42 +0200

    Changed the db Model

--- EnginXpress/EnginX_Final/Infinity/EnginX/EnginX/App_Start/RouteConfig.cs
+++ EnginXpress/EnginX_Final/Infinity/EnginX/EnginX/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EnginX
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "LT", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
